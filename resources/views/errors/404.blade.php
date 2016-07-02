434b9b1608f686f99b1f8296f8b916d28fcfff50
Ujjwal Ojha <bring2uo@gmail.com>
2016-07-02 19:13:25 +0545

    Basic layout

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+	<h1 class="title">Oops!</h1>
+	<h2 class="title">This is awkward.</h2>
+	<p>We encountered a 404 Not Found error.</p>
+	<p>
+		You are looking for something that doesn't exist or may have moved. Check out one of the links on this page
+		or head back to <a href="/">Home</a>.
+	</p>
+</div>
+
+@endsection
