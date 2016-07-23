4c20c2b9a0422d68ad50c5bbe9f8bd4020413cb3
Ujjwal Ojha <bring2uo@gmail.com>
2016-07-23 10:51:48 +0545

    list records on home page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PersonalDetailsController@listAll');
 
 Route::group(['prefix' => 'personal-details', 'as' => 'personal-details.'], function () {
     /*
